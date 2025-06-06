639702d4f2693e8e13579b7d0172a7b6ae844587
Ashish <ashishlalsingh19@gmail.com>
2025-06-06 16:07:18 +0545

    dashboard view updated

--- frontend/src/views/DashboardView.vue
+++ frontend/src/views/DashboardView.vue
@@ -1,10 +1,13 @@
 <template>
   <h1>Welcome {{ user?.first_name }}</h1>
+  <button type="submit" @click="handleLogout">Logout</button>
 </template>
 
 <script lang="ts" setup>
 import { ref, onMounted } from 'vue'
 import axios from '../lib/axios'
+import { useRouter } from 'vue-router'
+import { useAuthStore} from '../store/auth'
 
 interface User {
     id: Number
@@ -14,7 +17,8 @@ interface User {
 }
 
 const user = ref<User | null>(null)
-
+const router = useRouter()
+const auth = useAuthStore()
 onMounted(async() => {
     try{
         const res = await axios.get<User>('auth/users/me/')
@@ -23,6 +27,11 @@ onMounted(async() => {
         console.error('Failed to fetch user profile', err)
     }
 })
+
+const handleLogout =() => {
+    auth.logout()
+    router.push('/login')
+}
 
 </script>
 
