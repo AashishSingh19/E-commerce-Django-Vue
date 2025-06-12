e88b918f54693bc98d402fc8b83b9a1076bf5620
Ashish <ashishlalsingh19@gmail.com>
2025-06-12 16:05:25 +0545

    fix: correct foreign key for product

--- backend/cart/models.py
+++ backend/cart/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 from products.models import Product
 from django.conf import settings
+
 # Create your models here.
 class CartItem(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='cart_items')
