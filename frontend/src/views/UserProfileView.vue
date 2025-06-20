f29fc04b09377546f5cb968a6d8f83ae685d6a2f
Ashish <ashishlalsingh19@gmail.com>
2025-06-20 12:03:43 +0545

    feat(users): add user profile view

---
+++ frontend/src/views/UserProfileView.vue
@@ -0,0 +1,144 @@
+<template>
+<div class="user-profile">
+    <h1 class="account">My Account</h1>
+    <div class="profile-container">
+        <div class="user-details">
+            <h3>Account Details</h3>
+            <label for="first_name">
+                First Name:
+                <input type="text" class="firstname" v-model="firstname">
+            </label>
+            <label for="last_name">
+                Last Name:
+                <input type="text" class="lastname" v-model="lastname">
+            </label>
+            <label for="email">
+                Email:
+                <input type="email" class="email" v-model="email">
+            </label>
+            <label for="password">
+                Password:
+                <input type="password" class="password" v-model="password" placeholder="**********">
+            </label>
+            <button class="update" @click="updateProfile">Update</button>
+        </div>
+    </div>
+</div>
+</template>
+
+<script lang="ts" setup>
+import { onMounted, ref } from 'vue';
+import apiClient from '@/services/axios';
+import { useAuthStore } from '@/store/auth';
+
+const firstname = ref('')
+const lastname = ref('')
+const email = ref('')
+const password = ref('')
+// const authStore = useAuthStore()
+
+const fetchUserInfo = async() => {
+    try{
+        const token = localStorage.getItem('access')
+        const response = await apiClient.get('http://127.0.0.1:8000/api/users/profile/', {
+            headers: {
+                Authorization: `Bearer ${token}`
+            }
+        })
+        const data = response.data
+        firstname.value = data.first_name
+        lastname.value = data.last_name
+        email.value = data.email
+    } catch(error){
+        console.error('Failed to load user profile:', error)
+    }
+}
+onMounted(() => {
+    fetchUserInfo()
+})
+
+async function updateProfile(){
+
+}
+</script>
+
+<style>
+.user-profile{
+    max-width: 1200px;
+    margin: 0 auto; 
+    padding: 2rem 1rem 1rem;
+    font-family: 'Segoe UI', Roboto, sans-serif;
+    color: #2c3e50;
+}
+.user-profile h1 {
+  text-align: left;
+  font-size: 2rem;
+  font-weight: 600;
+  margin-top: 0; 
+  margin-bottom: 0.5rem; 
+  color: #1a1a1a;
+  padding-bottom: 0.5rem;
+  letter-spacing: 1px;
+}
+.profile-container {
+  display: flex;
+  gap: 2rem;
+  justify-content: space-between;
+  align-items: flex-start;
+  flex-wrap: wrap;
+  padding: 1rem;
+}
+
+.user-details{
+  flex: 1 1 48%;
+  max-width: 48%;
+  background-color: #ffffff;
+  padding: 1rem;
+  border-radius: 10px;
+  box-shadow: 0 4px 12px rgba(0, 0, 0, 0.05);
+  transition: all 0.3s ease-in-out;
+}
+.user-details h3 {
+  text-align: left;
+  margin-bottom: 1.5rem;
+  font-size: 1.5rem;
+  font-weight: 600;
+  color: #333;
+  border-bottom: 1px solid #eee;
+  padding-bottom: 0.5rem;
+}
+.user-details label {
+  display: block;
+  margin-bottom: 1.2rem;
+  font-size: 0.95rem;
+  color: #444;
+  font-weight: 500;
+}
+.user-details input{
+  width: 100%;
+  padding: 0.75rem 1rem;
+  border: 1px solid #ccc;
+  border-radius: 6px;
+  font-size: 1rem;
+  margin-top: 0.5rem;
+  background-color: #fdfdfd;
+  transition: border-color 0.3s;
+}
+.user-details input:focus {
+  border-color: #5a67d8;
+  outline: none;
+}
+.user-details button{
+  display: block;
+  width: 100%;
+  padding: 0.75rem;
+  margin-top: 1rem;
+  background-color: #4CAF50;
+  color: white;
+  border: none;
+  border-radius: 6px;
+  font-size: 1rem;
+  cursor: pointer;
+  transition: background-color 0.3s ease;
+}
+</style>
