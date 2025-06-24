670275938a3335ce83368895aef9b64584c45183
Ashish <ashishlalsingh19@gmail.com>
2025-06-24 10:50:29 +0545

    feat(cart): add create logic

--- backend/cart/views.py
+++ backend/cart/views.py
@@ -1,5 +1,5 @@
 from rest_framework import viewsets, status, permissions
-from rest_framework.views import APIView
+from rest_framework.response import Response
 from .models import CartItem
 from .serializers import CartItemSerializer
 
@@ -15,3 +15,21 @@ class CartItemViewSet(viewsets.ModelViewSet):
     
     def perform_create(self, serializer):
         serializer.save(user=self.request.user)
+
+    
+    def create(self, request, *args, **kwargs):
+        user = request.user
+        product_id = request.data.get('product_id')
+
+        if not product_id:
+            return Response({"detail": "Product ID is required."}, status=status.HTTP_400_BAD_REQUEST)
+
+        try:
+            cart_item = CartItem.objects.get(user=user, product_id=product_id)
+            cart_item.quantity += 1
+            cart_item.save()
+            serializer = self.get_serializer(cart_item)
+            return Response(serializer.data, status=status.HTTP_200_OK)
+
+        except CartItem.DoesNotExist:
+            return super().create(request, *args, **kwargs)
