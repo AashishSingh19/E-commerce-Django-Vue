2320e6641bc50c1ee7cddeadd072c539d2f07999
Ashish <ashishlalsingh19@gmail.com>
2025-06-24 14:33:13 +0545

    feat(products): add search filter logic to ProductListAPIView

--- backend/products/views.py
+++ backend/products/views.py
@@ -3,7 +3,8 @@ from rest_framework import generics
 from rest_framework.permissions import AllowAny
 from .models import Product, Category
 from .serializers import ProductSerializer, CategorySerializer
-
+from django_filters.rest_framework import DjangoFilterBackend
+from django.db.models import Q
 # Create your views here.
 
 class CategoryListAPIView(generics.ListAPIView):
@@ -18,9 +19,17 @@ class CategoryDetailAPIView(generics.RetrieveAPIView):
     permission_classes = [AllowAny]
 
 class ProductListAPIView(generics.ListAPIView):
-    queryset = Product.objects.all()
     serializer_class = ProductSerializer
     permission_classes = [AllowAny]
+
+    def get_queryset(self):
+        queryset = Product.objects.all()
+        query = self.request.query_params.get('q')
+        if query:
+            queryset = queryset.filter(
+                Q(name__icontains=query) | Q(category__name__icontains=query)
+            )
+        return queryset
 
 class ProductDetailAPIView(generics.RetrieveAPIView):
     queryset = Product.objects.all()
